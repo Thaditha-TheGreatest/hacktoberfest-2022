72c95e3cdfb33ff98c2baa53474fc011b1a4023c
Thaditha-The Greatest <104048696+Thaditha-TheGreatest@users.noreply.github.com>
2022-10-17 16:33:00 +0530

    Update sumOfTwoNumber.js

--- javascript/sumOfTwoNumber.js
+++ javascript/sumOfTwoNumber.js
@@ -1,7 +1,7 @@
 const sumoftwonumber = (num1, num2) => {
-    let total = 0;
+    let total = #;
     total = num1 + num2;
     return total;
 }
 
-console.log("Sum of Two Number = ", sumoftwonumber(10, 20))
+console.log("Sum of Two Number = ", sumoftwonumber(20, 20))
